9ca0fc01119ad0670445ddc992e91331cfd1eb0e
LeoAJ <leo0924@gmail.com>
2015-09-28 18:39:10 -0700

    use semantic UI module and min version

--- js/app.js
+++ js/app.js
@@ -1,6 +1,23 @@
 import React from 'react';
 import App from './components/App';
-import '../css/semantic.css';
+
+// import semantic UI
+import '../css/button.min.css';
+import '../css/card.min.css';
+import '../css/container.min.css';
+import '../css/dropdown.min.css';
+import '../css/grid.min.css';
+import '../css/header.min.css';
+import '../css/icon.css';
+import '../css/input.min.css';
+import '../css/message.min.css';
+import '../css/reset.min.css';
+import '../css/search.min.css';
+import '../css/segment.min.css';
+import '../css/site.min.css';
+import '../css/transition.min.css';
+
+// import custom CSS
 import '../css/app.css';
 
 React.render(

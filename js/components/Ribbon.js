85e33112b06c9d40846e0f915442707c38899350
LeoAJ <leo0924@gmail.com>
2016-04-01 12:10:21 -0700

    use aphrodite and remove radium

--- js/components/Ribbon.js
+++ js/components/Ribbon.js
@@ -1,24 +1,26 @@
-import React from 'react';
-import radium from 'radium';
+import React, { Component } from 'react';
 import ribbon from '../../css/img/ribbon.png';
+import { StyleSheet, css } from 'aphrodite';
 
-const style = {
-  position: 'absolute',
-  top: 0,
-  right: 0,
-  border: 0,
-  zIndex: 100,
-  '@media (max-width: 768px)': {
-    display: 'none'
+const style = StyleSheet.create({
+  ribbon: {
+    position: 'absolute',
+    top: 0,
+    right: 0,
+    border: 0,
+    zIndex: 100,
+    '@media (max-width: 768px)': {
+      display: 'none'
+    }
   }
-};
+});
 
 const Ribbon = () => (
   <a target="_blank" href="https://github.com/LeoAJ/react-iTunes-search">
     <img src={ribbon.indexOf('dist') === -1
               ? `dist/${ribbon}`
-              : ribbon.substring(1)} style={style} />
+              : ribbon.substring(1)} className={css(style.ribbon)} />
   </a>
 );
 
-export default radium(Ribbon);
+export default Ribbon;

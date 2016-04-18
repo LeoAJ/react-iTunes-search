ff72a70dc37b48a224323f3a555dbd265f6f430f
LeoAJ <leo0924@gmail.com>
2016-04-18 10:21:42 -0700

    add jss

--- js/components/Ribbon.js
+++ js/components/Ribbon.js
@@ -1,26 +1,32 @@
 import React, { Component } from 'react';
 import ribbon from '../../css/img/ribbon.png';
-import { StyleSheet, css } from 'aphrodite';
+import jss from 'jss';
 
-const style = StyleSheet.create({
+const style = {
   ribbon: {
     position: 'absolute',
     top: 0,
     right: 0,
     border: 0,
-    zIndex: 100,
-    '@media (max-width: 768px)': {
+    'z-index': 100
+  },
+  '@media (max-width: 768px)': {
+    ribbon: {
       display: 'none'
     }
   }
-});
+};
+
+const { classes } = jss.createStyleSheet(style).attach();
 
 const Ribbon = () => (
   <a target="_blank" href="https://github.com/LeoAJ/react-iTunes-search">
-    <img alt="img" src={
+    <img alt="img"
+      className={classes.ribbon}
+      src={
       ribbon.indexOf('dist') === -1
         ? `dist/${ribbon}`
-        : ribbon.substring(1)} className={css(style.ribbon)
+        : ribbon.substring(1)
       }
     />
   </a>
